dc2e248f3a238bea0889991b15044b2ef6ee1a3e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-12 23:05:31 +0600

    include tasks

--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>wellcome to the task manager</h1>")
+
+def show_task(request):
+    return HttpResponse("list of all your task")
