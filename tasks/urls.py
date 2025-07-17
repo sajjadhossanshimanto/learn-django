9ca94f7cc93b80429d46546711550cd923876e46
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-17 21:40:30 +0600

    user_dashboard

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import manager_dashboard
+from tasks.views import manager_dashboard, user_dashboard
 
 
 urlpatterns = [
     path("manager_dashboard", manager_dashboard),
+    path("user_dashboard", user_dashboard),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def manager_dashboard(request):
     return render(request, "dashboard.html")
+
+def user_dashboard(request):
+    return render(request, "user_dashboard.html")
