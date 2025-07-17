4c92ae92ad560bbc28b8a80d172e67fe15aea860
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-17 21:36:49 +0600

    app temlate setup

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from tasks.views import show_task, show_specific_task
+from tasks.views import manager_dashboard
 
 
 urlpatterns = [
-    path("show_task", show_task),
-    path("show_task/<_id>", show_specific_task),
+    path("manager_dashboard", manager_dashboard),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,11 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home(request):
+def manager_dashboard(request):
     return render(request, "dashboard.html")
-
-def show_task(request):
-    return HttpResponse("list of all your task")
-
-def show_specific_task(request, _id):# _id var name have tobe exact
-    return HttpResponse(f"details of your task {_id}")
