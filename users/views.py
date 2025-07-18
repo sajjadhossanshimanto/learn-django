4a1c543ebf0b73bf9b14dfb92fe7c30e63039bb1
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-18 16:06:31 +0600

    for loop in django

--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {
-        "name": [1, 2, 3, 4, 5]
+        "nums": [1, 2, 3, 4, 5],
+        "name": "shimnato",
     }
     return render(request, "test.html", context=context)
 
