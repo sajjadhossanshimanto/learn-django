09951f63b450cb5d5ff798b6e00c578154239f34
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-21 23:05:38 +0600

    static -> dynamic page

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.models import Task
 
 
 # Create your views here.
 def manager_dashboard(request):
-    return render(request, "manager_dashboard.html")
+    task = Task.objects.all()
+    
+    context = {
+        'tasks': task,
+        'total_task': task.count(),
+        'completed_task': Task.objects.filter(is_completed=True).count(),
+        'in_progress_task': Task.objects.filter(status='IN_PROGRESS').count(),
+    }
+    return render(request, "manager_dashboard.html", context=context)
 
 def user_dashboard(request):
     return render(request, "user_dashboard.html")
