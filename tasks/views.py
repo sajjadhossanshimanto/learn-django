941664537eae35638fdd44d623a71262ac359c90
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-22 22:42:28 +0600

    query obtimisation

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.models import Task
+from django.db.models import Count, Q
 
 
 # Create your views here.
 def manager_dashboard(request):
-    task = Task.objects.all()
-    
+    task = Task.objects.select_related('details').prefetch_related("assigned_to").all()
+    count = Task.objects.aggregate(
+        total_task = Count('id'),
+        completed_task = Count('id', filter=Q(is_completed=True)),
+        in_progress_task = Count('id', filter=Q(status='IN_PROGRESS')),
+    )
+
     context = {
         'tasks': task,
-        'total_task': task.count(),
-        'completed_task': Task.objects.filter(is_completed=True).count(),
-        'in_progress_task': Task.objects.filter(status='IN_PROGRESS').count(),
+        'total_task': count['total_task'],
+        'completed_task': count['completed_task'],
+        'in_progress_task': count['in_progress_task'],
     }
     return render(request, "manager_dashboard.html", context=context)
 
