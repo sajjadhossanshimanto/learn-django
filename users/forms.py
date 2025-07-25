e49d6275817042c4c8eb45887c5240613cbbca73
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-07-25 23:11:03 +0600

    non field form validation

--- users/forms.py
+++ users/forms.py
@@ -24,9 +24,25 @@ class CustomRegistrationForm(forms.ModelForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'password1', 'confirm_password', 'email']
 
-    def clean_password1(self):
+    def clean_password1(self):# clean_*field-name*
         print('clean running')
         p1 = self.cleaned_data.get('password1')
 
         if not re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', p1):
+            'NOTE: ValidationError can accept a string and list also dict'
             raise forms.ValidationError('password must include Uppercase, lowercase, number and special charecter')
+        
+        return p1 # NOTE: cummon error
+
+    def clean(self):
+        '''
+        NOTE: ultrimately i can use this function for both field and non-field
+        '''
+        cleaned_data = self.cleaned_data
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('confirm_password')
+
+        if p1!=p2:
+            raise forms.ValidationError('password do not match')
+        
+        return cleaned_data
