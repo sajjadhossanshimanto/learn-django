e6289a091bba0f80c6f5a857a64fc56477eda146
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-08-04 15:58:17 +0600

    un-authorized page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from core.views import no_permission
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("task/", include("tasks.urls")),
-    path("users/", include("users.urls"))
+    path("users/", include("users.urls")),
+    path("unauthurized/", no_permission),
 ] + debug_toolbar_urls()
