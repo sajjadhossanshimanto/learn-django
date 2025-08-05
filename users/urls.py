d4d4b28f61f416f6ef1689b22dff2f6f7c38bd04
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-08-05 14:10:26 +0600

    change rule button for user

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', sign_out, name="logout"),
     path('activate/<int:user_id>/<str:token>/', activate_user),
     path('admin/dashboard/', admin_dashboard, name="admin-dashboard"),
-    path('admin/<int:user_id>/assign-role/', assign_rule),
+    path('admin/<int:user_id>/assign-role/', assign_rule, name="assign_role"),
     path('admin/group-list/', group_list, name="list-group"),
     path('admin/create-group/', create_group, name='create-group')
 ]
