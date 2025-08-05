b4aaf8b0c862a99a9f5da1635b2bdb44f93a14ee
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-08-05 18:10:01 +0600

    employee model replaced with User

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from tasks.models import Task, TaskDetail, Employee
+from tasks.models import Task, TaskDetail
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(TaskDetail)
-admin.site.register(Employee)
+admin.site.register(User)
--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.name
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -23,7 +16,7 @@ class Task(models.Model):
         on_delete=models.CASCADE,
         default=1
     )
-    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
+    assigned_to = models.ManyToManyField(User, related_name='tasks')
     title = models.CharField(max_length=250)
     due_date = models.DateField()
     # is_completed = models.BooleanField(default=False)# status=completed does the same
